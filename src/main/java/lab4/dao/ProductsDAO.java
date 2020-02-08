6fa6afed23b81f198e04e1eb0f0f85d6c8a2f1f6
Vlad Zybkin <zybkin11@gmail.com>
2020-02-08 19:44:57 +0300

    added drop and create operations to DAO

--- src/main/java/lab4/dao/ProductsDAO.java
+++ src/main/java/lab4/dao/ProductsDAO.java
@@ -1,5 +1,25 @@
 package lab4.dao;
 
-public class ProductsDAO
-{
+import lab4.executor.Executor;
+
+import java.sql.Connection;
+
+public class ProductsDAO {
+
+    private final Executor executor;
+
+    public ProductsDAO(Connection connection) {
+        executor = new Executor(connection);
+    }
+
+
+    public void createTable() {
+        executor.executeUpdate("CREATE TABLE products (id INT AUTO_INCREMENT, prodID INT NOT NULL, " +
+                "title VARCHAR(256) NOT NULL, cost INT NOT NULL, PRIMARY KEY (id))");
+    }
+
+    public void dropTable() {
+        executor.executeUpdate("DROP TABLE products");
+    }
+
 }
