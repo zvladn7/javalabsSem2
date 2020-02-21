efed0d5416b89936d10bb7809198d7d4c1fcbc28
Vlad Zybkin <zybkin11@gmail.com>
2020-02-21 17:17:15 +0300

    deleted all code because I hava some troubles with idea project so the next commit will be recover the structer of lab4

--- src/lab4/src/main/java/lab4/executor/ResultHandler.java
+++
@@ -1,8 +0,0 @@
-package main.java.lab4.executor;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public interface ResultHandler<T> {
-    T handle(ResultSet resultSet) throws SQLException;
-}
